2b7ad866f06a6bdb95f5faf53bef10294c74f40a
jimsmith <jim.smith@gmail.com>
2019-05-02 14:57:54 +0100

    add fetch and render

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ require('dotenv').config()
 const request = require('request-promise')
 const http = require('http')
 const fs = require('fs')
+const hour_diff = require('date-fns/difference_in_hours')
+const compare_asc = require('date-fns/compare_asc')
+
+
+const template = fs.readFileSync('./html/git_status.html', 'utf-8').split('\n')
+
 
 let server_started = false
 
@@ -10,10 +16,55 @@ let counter = 0
 
 const repo = `https://api.github.com/repos/${process.env.REPO}/pulls`
 
+const sort_by_comments_desc = (one, two) => {
+  if (one.comments > two.comments) {
+    return -1
+  } else if (one.comments < two.comments) {
+    return 1
+  }
+  return 0
+}
+
+const sort_by_date_desc = (one, two) => {
+  let one_date = new Date(one.created_at)
+  let two_date = new Date(two.created_at)
+  return compare_asc(one_date, two_date)
+}
+
+const process_pull_data = (pulls) => {
+  const max_comments_pull = pulls.sort(sort_by_comments_desc)[0]
+  const oldest = pulls.sort(sort_by_date_desc)[0]
+  const total_comments = pulls.reduce((acc, pull) => { return acc + parseInt(pull.comments, 10)}, 0)
+  const average = total_comments / pulls.length
+  const now = new Date()
+  const total_hours = pulls.reduce((acc,  pull) => {return acc + hour_diff(now, pull.created_at)}, 0)
+  const ave_age = ((total_hours/pulls.length)/24).toFixed(2)
+  const replaced = []
+
+  template.forEach((li) => {
+      replaced.push(
+        li
+          .replace('${repo}', process.env.REPO)
+          .replace('${num_open_prs}', pulls.length)
+          .replace('${num_ave_age}', ave_age)
+          .replace('${most_comments_title}', max_comments_pull.title)
+          .replace('${author_most_comments_avi}', max_comments_pull.owner_avatar)
+          .replace('${author_most_comments}', max_comments_pull.owner)
+          .replace('${most_comments_comments}', max_comments_pull.comments)
+          .replace('${oldest_title}', oldest.title)
+          .replace('${author_oldest_avi}', oldest.owner_avatar)
+          .replace('${author_oldest}', oldest.owner)
+          .replace('${oldest_age}', (hour_diff(new Date(), new Date(oldest.created_at))/24).toFixed(2))
+      )
+    }
+  )
+  return replaced.join('\n')
+}
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end(JSON.stringify(pullData));
+  res.end(process_pull_data(pullData));
 })
 
 const headers = {
@@ -41,8 +92,7 @@ const getPulls = (pulls) => {
           created_at: data.created_at,
           owner: data.user.login,
           owner_avatar: data.user.avatar_url,
-          comments: data.comments,
-          review_comments: data.review_comments,
+          comments: data.comments + data.review_comments,
         })
         counter++
         if (counter === pulls.length) {
@@ -62,4 +112,4 @@ authRequest(repo)
 .then(res => {
   const pulls = JSON.parse(res)
   getPulls(pulls)
-}).then(() => console.log(pullData))
+}).then(() => console.log('Request done'))
